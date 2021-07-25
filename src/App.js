c5043554247cb3c6be66829a67e94cd9f7da74e6
Joshua Mukisa <kiryowajoshua22@gmail.com>
2021-07-25 15:42:33 +0300

    Commited

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Redirect } from 'react-router-dom';
+import AllQuotes from './pages/AllQuotes.js';
+import NewQuote from './pages/NewQuote';
+import QuoteDetail from './pages/QuoteDetail';
+import React from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Route exact path="/">
+        <Redirect to="/quotes" />
+      </Route>
+      <Route exact path="/quotes">
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </React.Fragment>
   );
 }
 
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function QuoteDetail() {
+    return (
+        <h1>Quote detail page</h1>
+    )
+}
+
+export default QuoteDetail
